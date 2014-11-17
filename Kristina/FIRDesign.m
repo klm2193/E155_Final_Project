563c86007f4772fa443405e8114c46e08cdd72ec
Kristina Ming <kming@hmc.edu>
2014-11-17 12:53:44 -0800

    Worked More on Filter Design
    Looks like low order band pass filters are basically low pass filters
    for the frequency ranges we want...

--- Kristina/FIRDesign.m
+++ Kristina/FIRDesign.m
@@ -1,11 +1,17 @@
-f = [0 0.6 0.6 1];
-m = [1 1 0 0];
-b = fir2(30,f,m);
-[h,w] = freqz(b,1,128);
+Ts = 0.002;
+Wn = [1*(2*pi*Ts/pi) 3*(2*pi*Ts/pi)];
+b = fir1(100,Wn, 'bandpass');
+[h,w] = freqz(b,1,1000);
 
-plot(f,m,w/pi,abs(h))
+% f = [0 0.6 0.6 0.8 0.8 1];
+% m = [0 0 1 1 0 0 ];
+% b = fir2(30,f,m);
+% [h,w] = freqz(b,1,128);
+
+% plot(f, m, w/pi,abs(h))
+plot(w/pi,abs(h))
 xlabel('Normalized Frequency (\times\pi rad/sample)')
 ylabel('Magnitude')
-legend('Ideal','fir2 designed')
-legend boxoff
+% legend('Ideal','fir2 designed')
+% legend boxoff
 title('Comparison of Frequency Response Magnitudes')
