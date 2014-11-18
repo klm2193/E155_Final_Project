328f38b3bd7ab30da7905f502d7868900ad1e3b1
Kristina Ming <kming@hmc.edu>
2014-11-17 23:08:41 -0800

    Added C Code for ADC
    Taken from lecture

--- Kristina/FIRDesign.m
+++ Kristina/FIRDesign.m
@@ -1,7 +1,16 @@
-Ts = 0.01;
-Wn = [1*(2*pi*Ts/pi) 3*(2*pi*Ts/pi)];
-b = fir1(40, Wn, 'bandpass');
+Ts = 0.05;
+w1 = 1*(2*pi*Ts/pi);
+w2 = 3*(2*pi*Ts/pi);
+Wn = [w1]%[w1 w2]
+% b = butter(40, Wn, 'bandpass');
+b = fir1(30, Wn, 'bandpass');
 [h,w] = freqz(b,1,1000);
+
+f = [0 w1 w1 w2 w2 1];
+m = [0 0 1 1 0 0];
+
+f = [0 w1 w1 1];
+m = [1 1 0 0];
 
 % f = [0 0.6 0.6 0.8 0.8 1];
 % m = [0 0 1 1 0 0 ];
@@ -9,10 +18,10 @@ b = fir1(40, Wn, 'bandpass');
 % [h,w] = freqz(b,1,128);
 
 % plot(f, m, w/pi,abs(h))
-plot(w/pi,abs(h))
+plot(f,m,w/pi,abs(h))
 xlabel('Normalized Frequency (\times\pi rad/sample)')
 ylabel('Magnitude')
-% legend('Ideal','fir2 designed')
+legend('Ideal','fir1 designed')
 % legend boxoff
 title('Comparison of Frequency Response Magnitudes')
 grid on
---
+++ adc.c
@@ -0,0 +1,22 @@
+#include <P32xxxx.h>
+void initadc(int channel) {
+	AD1CHSbits.CHOSA = channel; // select which channel
+	AD1PCFGCLR = 1 << channel; // configure input pin
+	AD1CON1bits.ON = 1; // turn ADC on
+	AD1CON1bits.SAMP = 1; // begin sampling
+	AD1CON1bits.DONE = 0; // clear DONE flag
+}
+
+int readadc(void) {
+	AD1CON1bits.SAMP = 0; // end sampling, star conversion
+	while (!AD1CON1bits.DONE); // wait until DONE
+	AD1CON1bits.SAMP = 1; // resume sampling
+	AD1CON1bits.DONE = 0; // clear DONE flag
+	return ADC1BUF0; // return result
+}
+
+int main(void) {
+	int sample;
+	initadc(11);
+	sample = readadc();
+}
