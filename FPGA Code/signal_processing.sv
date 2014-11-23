6bb134b230520ccff2564837334ee140dfd7b4ce
musisat <kkimberling@hmc.edu>
2014-11-22 19:31:48 -0800

    Started to customize SystemVerilog code

--- FPGA Code/signal_processing.sv
+++ FPGA Code/signal_processing.sv
@@ -1,5 +1,13 @@
+
+
+module finalProject(input logic clk, sck, sdo, reset,
+						  output logic sdi);
+	signal_processing sp(clk, reset);
+	spi_slave ss(sck, sdo, sdi, reset, d, q)
+endmodule
+
 // signal processing code for FPGA
-module signal_processing(input logic clk);
+module signal_processing(input logic clk, reset);
 
 endmodule
 
@@ -7,9 +15,9 @@ module spi_slave(input logic sck, // from master
 					  input logic sdo, // from master
 					  output logic sdi, // to master
 					  input logic reset,
-					  input logic [31:0] d, // data to send 
-					  output logic [31:0] q, // data received
-					  output logic [15:0] xCoor, yCoor); // cursor x and y coordinates
+					  input logic [9:0] d, // data to send 
+					  output logic [9:0] q, // data received
+					  output logic [9:0] voltage); // discrete output signal
 
 	logic [4:0] cnt; 
 	logic qdelayed;
@@ -24,15 +32,13 @@ module spi_slave(input logic sck, // from master
 	// loads d at the start, shifts sdo into bottom position on subsequent step 
 	always_ff @(posedge sck)
 		begin
-			q <= (cnt == 0) ? d : {q[30:0], sdo};
-			xCoor <= (cnt == 0) ? q[31:16] : xCoor;
-			yCoor <= (cnt == 0) ? q[15:0] : yCoor;
+			q <= (cnt == 0) ? d : {q[8:0], sdo};
 		end
 
 	// align sdi to falling edge of sck // load d at the start
 	always_ff @(negedge sck)
-		qdelayed = q[31];
+		qdelayed = q[9];
 
-	assign sdi = (cnt == 0) ? d[31] : qdelayed;
+	assign sdi = (cnt == 0) ? d[9] : qdelayed;
 
 endmodule
