ca753826b4d0cd939fe61308de7f3b4198752ac2
musisat <kkimberling@hmc.edu>
2014-11-22 22:55:50 -0800

    Redo of FPGA

--- FPGA Code/signal_processing.sv
+++ FPGA Code/signal_processing.sv
@@ -1,25 +1,18 @@
-
-
-module finalProject(input logic clk, sck, sdo, reset,
-						  output logic sdi);
-	signal_processing sp(clk, reset);
-	spi_slave ss(sck, sdo, sdi, reset, d, q)
-endmodule
-
 // signal processing code for FPGA
-module signal_processing(input logic clk, reset,
+module signal_processing(input logic clk, reset, sck, sdo,
 								 input logic [9:0] voltage,
 								 input logic [29:0] a, // FIR filter coefficients
+								 output logic, sdi,
 								 output logic [9:0] filtered);
-	
+	spi_slave ss(sck,sdo,sdi,reset,d,q,voltage);
 endmodule
 
 module spi_slave(input logic sck, // from master 
 					  input logic sdo, // from master
 					  output logic sdi, // to master
 					  input logic reset,
-					  input logic [9:0] d, // data to send 
-					  output logic [9:0] q, // data received
+					  input logic [31:0] d, // data to send 
+					  output logic [31:0] q, // data received
 					  output logic [9:0] voltage); // discrete output signal
 
 	logic [4:0] cnt; 
@@ -35,13 +28,14 @@ module spi_slave(input logic sck, // from master
 	// loads d at the start, shifts sdo into bottom position on subsequent step 
 	always_ff @(posedge sck)
 		begin
-			q <= (cnt == 0) ? d : {q[8:0], sdo};
+			q <= (cnt == 0) ? d : {q[30:0], sdo};
+			voltage <= (cnt == 0) ? q[9:0] : voltage;
 		end
 
 	// align sdi to falling edge of sck // load d at the start
 	always_ff @(negedge sck)
-		qdelayed = q[9];
+		qdelayed = q[31];
 
-	assign sdi = (cnt == 0) ? d[9] : qdelayed;
+	assign sdi = (cnt == 0) ? d[31] : qdelayed;
 
 endmodule
