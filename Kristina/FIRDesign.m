b65babe9a290bae7f743abf5f314edb4764813a8
Kristina Ming <kming@hmc.edu>
2014-11-23 19:57:36 -0800

    Played with filter values

--- Kristina/FIRDesign.m
+++ Kristina/FIRDesign.m
@@ -1,15 +1,15 @@
 Ts = 0.05;
-w1 = 1*(2*pi*Ts/pi);
-w2 = 3*(2*pi*Ts/pi);
-Wn = [w1]%[w1 w2]
+w1 = 0.5*(2*pi*Ts/pi)
+w2 = 3.5*(2*pi*Ts/pi)
+Wn = [w2]%[w1 w2]
 % b = butter(40, Wn, 'bandpass');
-b = fir1(30, Wn, 'bandpass');
+b = fir1(30, Wn);%, 'bandpass');
 [h,w] = freqz(b,1,1000);
 
 f = [0 w1 w1 w2 w2 1];
 m = [0 0 1 1 0 0];
 
-f = [0 w1 w1 1];
+f = [0 w2 w2 1];
 m = [1 1 0 0];
 
 % f = [0 0.6 0.6 0.8 0.8 1];
