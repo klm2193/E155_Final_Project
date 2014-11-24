8d1175cde6b14052d2820f3fdf7493998b306872
musisat <kkimberling@hmc.edu>
2014-11-23 19:21:03 -0800

    Added shift register for voltage values

--- FPGA Code/signal_processing.sv
+++ FPGA Code/signal_processing.sv
@@ -13,11 +13,43 @@ module filter(input logic clk, reset,
 	logic [31:0] a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
 	all, a12, a13, a14, a15, a16, a17, a18, a19, a20,
 	a21, a22, a23, a24, a25, a26, a27, a28, a29, a30;	
-	logic [31:0] filteredSignal;
+	logic [9:0] v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10,
+	vll, v12, v13, v14, v15, v16, v17, v18, v19, v20,
+	v21, v22, v23, v24, v25, v26, v27, v28, v29, v30;	
+	logic [9:0] filteredSignal;
 	
 	always_ff(@posedge clk)
 		begin
-			
+			v0 <= v1;
+			v1 <= v2;
+			v2 <= v3;
+			v3 <= v4;
+			v4 <= v5;
+			v5 <= v6;
+			v6 <= v7;
+			v7 <= v8;
+			v8 <= v9;
+			v9 <= v10;
+			v10 <= v11;
+			v11 <= v12;
+			v12 <= v13;
+			v13 <= v14;
+			v14 <= v15;
+			v15 <= v16;
+			v16 <= v17;
+			v17 <= v18;
+			v18 <= v19;
+			v19 <= v20;
+			v20 <= v21;
+			v21 <= v22;
+			v22 <= v23;
+			v23 <= v24;
+			v24 <= v25;
+			v25 <= v26;
+			v26 <= v27;
+			v27 <= v28;
+			v28 <= v29;
+			v29 <= v30;
 		end
 	
 
