1f4269d4e687172e05d1358ebe536a8a2fbcf679
musisat <kkimberling@hmc.edu>
2014-11-26 22:01:05 -0800

    Made filter design more general

--- Katie/ImplementFIR.m
+++ Katie/ImplementFIR.m
@@ -3,10 +3,11 @@ function [ filtered ] = ImplementFIR( VoltIn, fs )
 % the input signal to filter it.
 
 fsdown = 200;
+ts = 1/fsdown;
 VoltDown = downsample(VoltIn,fs/fsdown);
 len = length(VoltDown);
 t = 0:1/fsdown:len/fsdown;
-b = fir1(30,0.35);
+b = fir1(30,0.35*2*pi*ts);
 filtered = conv(VoltIn,b);
 hold off
 plot(t(1:len),filtered(1:len))
