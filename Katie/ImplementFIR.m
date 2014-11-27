916fac5387ae8d97e0112a7aaea1119374bb3c92
musisat <kkimberling@hmc.edu>
2014-11-26 21:53:44 -0800

    Added downsampling if we need it

--- Katie/ImplementFIR.m
+++ Katie/ImplementFIR.m
@@ -2,8 +2,10 @@ function [ filtered ] = ImplementFIR( VoltIn, fs )
 %IMPLEMENTFIR will calculate the FIR filter coefficients and convolve with
 % the input signal to filter it.
 
-len = length(VoltIn);
-t = 0:1/fs:len/fs;
+fsdown = 200;
+VoltDown = downsample(VoltIn,fs/fsdown);
+len = length(VoltDown);
+t = 0:1/fsdown:len/fsdown;
 b = fir1(30,0.35);
 filtered = conv(VoltIn,b);
 hold off
