6d0f2e3c721a44bca9539bb631bf3e09c6271232
musisat <kkimberling@hmc.edu>
2014-11-26 21:28:55 -0800

    Added MATLAB filter files and oscilloscope data

---
+++ Katie/lowPassPulse.m
@@ -0,0 +1,15 @@
+function  lowPassPulse( VoltIn, fs, fc )
+% LOWPASSPULSE takes in a heartbeat signal and downsamples it to 20 Hz and
+% lowpasses it with a cutoff frequency of fc.
+
+fs2 = 20;
+downfactor = fs/fs2;
+VoltDown = downsample(VoltIn, downfactor);
+hold off
+[y,t] = lowPass(VoltDown, fs2, fc);
+plot(t,VoltDown,'r')
+hold on
+plot(t,y)
+hold off
+end
+
