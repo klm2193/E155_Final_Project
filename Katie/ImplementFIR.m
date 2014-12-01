bfd51653ee7506c916547f0f0e05debf697fb682
musisat <kkimberling@hmc.edu>
2014-12-01 10:36:04 -0800

    Changed plotting of FIR filter
    And more thumbs...

--- Katie/ImplementFIR.m
+++ Katie/ImplementFIR.m
@@ -2,15 +2,20 @@ function [ filtered ] = ImplementFIR( VoltIn, fs )
 %IMPLEMENTFIR will calculate the FIR filter coefficients and convolve with
 % the input signal to filter it.
 
+tin = 0:1/fs:(length(VoltIn)-1)/fs;
+fc = 3;
 fsdown = 200;
 ts = 1/fsdown;
 VoltDown = downsample(VoltIn,fs/fsdown);
 len = length(VoltDown);
 t = 0:1/fsdown:len/fsdown;
-b = fir1(30,0.35*2*pi*ts);
+b = fir1(30,fc*2*pi*ts);
 filtered = conv(VoltIn,b);
 hold off
-plot(t(1:len),filtered(1:len))
+plot(tin,VoltIn,'r')
+hold on
+plot(t,filtered(15:len+15),'b')
+hold off
 
 end
 
