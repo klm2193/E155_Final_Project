4a3edcaeca6ab2de452f3edc0cbe88374dd0976a
Kristina Ming <kming@hmc.edu>
2014-12-02 18:41:29 -0800

    Added sum for find peaks slope values
    Updated comments for the FIR filter and added code to sum the left and
    right half of the shift register holding the slope changes in the find
    peaks algorithm

--- FPGA Code/signal_processing.sv
+++ FPGA Code/signal_processing.sv
@@ -11,15 +11,19 @@ endmodule
 module filter(input logic clk, reset,
 			  input logic [9:0] voltage,
 			  output logic [9:0] filtered);
+			  
+	// filter coefficients
 	logic [31:0] a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
 	logic [31:0] a11, a12, a13, a14, a15;
 	
+	// delayed voltage values
 	logic [9:0] v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10;
 	logic [9:0] v11, v12, v13, v14, v15, v16, v17, v18, v19, v20;
 	logic [9:0] v21, v22, v23, v24, v25, v26, v27, v28, v29, v30;	
 	
 	logic [9:0] filteredSignal;
 	
+	// assign FIR filter coefficients
 	always_comb
 		begin
 			a0 = 0.0032;
@@ -40,6 +44,7 @@ module filter(input logic clk, reset,
 			a15 = 0.0661;
 		end
 	
+	// shift register to delay the voltage signal
 	always_ff @(posedge clk)
 		begin
 			v0 <= v1;
@@ -73,6 +78,7 @@ module filter(input logic clk, reset,
 			v28 <= v29;
 			v29 <= v30;
 			
+			// calculate the filtered signal
 			filteredSignal <= a0*(v0+v30) + a1*(v1+v29) + a2*(v2+v28) + a3*(v3+v27) + a4*(v4+v26) + a5*(v5+v25) +
 							  a6*(v6+v24) + a7*(v7+v23) + a8*(v8+v22) + a9*(v9+v21) + a10*(v10+v20) + a11*(v11+v19) +
 							  a12*(v12+v18) + a13*(v13+v17) + a14*(v14+v16) + a15*v15;
@@ -115,13 +121,13 @@ module spi_slave(input logic sck, // from master
 	
 endmodule
 
-// module to find peaks
+// module to find the peaks of a signal
 module findPeaks(input  logic clk, reset,
 				 input  logic[9:0] newSample, oldSample,
 				 output logic numPeaks);
 				 
 	logic[9:0] newDifference;
-	logic[99:0] s; // shift register to track slope change
+	logic[99:0] s; // shift register (buffer) to track slope change
 	logic[9:0] leftSum, rightSum;
 	
 	// keep track of if the slope is increasing or decreasing
@@ -141,6 +147,12 @@ module findPeaks(input  logic clk, reset,
 				
 			// shift in the new indicator bit
 			s <= {s[98:0], newDifference};
+			
+			// keep track of the sum of the left and right sides of
+			// the shift register
+			rightSum <= rightSum + newSample - s[49];
+			leftSum <= leftSum + s[49] - s[99];
+			
 		end
 				 
 endmodule
