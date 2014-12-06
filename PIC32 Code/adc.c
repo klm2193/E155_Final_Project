a7b381a4bcb02c4a97cb7c8215438441a396a022
Kristina Ming <kming@hmc.edu>
2014-12-05 16:32:27 -0800

    Added in configuration for timers for speaker
    Needs to be changed later because we have a different peripheral clock

--- PIC32 Code/adc.c
+++ PIC32 Code/adc.c
@@ -18,7 +18,7 @@ int readadc(void) ;
 void initTimers(void) {
 	
 	//	Assumes peripheral clock at 10MHz
-
+	
 	//	Use Timer3 for frequency generation	
 	//	T3CON
 	//	bit 15: ON = 1: enable timer
@@ -32,6 +32,40 @@ void initTimers(void) {
 	//	bit 1:	TCS = 0: use internal peripheral clock
 	//	bit 0:	unused
 	T3CON = 0b1000000000110000;
+	
+	// Timers for speaker
+	//	Assumes peripheral clock at 5MHz
+	
+	// 	Use Timer1 for note duration
+	//	T1CON
+	//	bit 15: ON=1: enable timer
+	//	bit 14: FRZ=0: keep running in exception mode
+	//	bit 13: SIDL = 0: keep running in idle mode
+	//	bit 12: TWDIS=1: ignore writes until current write completes
+	//	bit 11: TWIP=0: don't care in synchronous mode
+	//	bit 10-8: unused
+	//	bit 7:	TGATE=0: disable gated accumulation
+	//	bit 6:	unused
+	//	bit 5-4: TCKPS=11: 1:256 prescaler
+	//	bit 3:	unused
+	//	bit 2:	don't care in internal clock mode
+	// 	bit 1:	TCS=0: use internal peripheral clock
+	//	bit 0:	unused
+	T1CON = 0b1001000000110000;
+
+	//	Use Timer2 for frequency generation	
+	//	T2CON
+	//	bit 15: ON=1: enable timer
+	//	bit 14: FRZ=0: keep running in exception mode
+	//	bit 13: SIDL = 0: keep running in idle mode
+	//	bit 12-8: unused
+	//	bit 7: 	TGATE=0: disable gated accumulation
+	//	bit 6-4: TCKPS=010: 1:4 prescaler
+	//	bit	3:	T32=0: 16-bit timer
+	//	bit 2:	unused
+	//	bit 1:	TCS=0: use internal peripheral clock
+	//	bit 0:	unused
+	T2CON = 0b1000000000100000;
 }
 
 // intialize SPI
